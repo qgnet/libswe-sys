764a277bbae27a5472112a9b5898527b702531a3
qgnet <105511815+qgnet@users.noreply.github.com>
2023-04-19 07:57:45 +0000

    update uoj_newbabyE

---
+++ src/ProgrammingA4/ProgrammingA4.java
@@ -0,0 +1,8 @@
+
+package ProgrammingA4;
+
+/*
+* Author: Max Carr, Erin Cook, Zac Sliker
+* Date: 4/13/2018 
+* Overview: Runs a Dynamic Algorithm to determine lowest possible number of coins needed to get the total.
+*/
