aa6b2935f1e09891ac8e892d7f961b209584e551
qgnet <105511815+qgnet@users.noreply.github.com>
2023-04-19 11:53:10 +0000

    Updates from ShareLaTeX

--- src/ProgrammingA4/ProgrammingA4.java
+++ src/ProgrammingA4/ProgrammingA4.java
@@ -6,3 +6,13 @@ package ProgrammingA4;
 * Date: 4/13/2018 
 * Overview: Runs a Dynamic Algorithm to determine lowest possible number of coins needed to get the total.
 */
+
+public class ProgrammingA4 {
+	public static int p(int[] change, int a) {
+////////////////////////////////////Checking for array of 0's//////////////////////////////////////////////
+                int p = 0;
+                for(int k = 0; k < change.length; k++){
+                    p += change[k];
+                }
+                if(p == 0){
+                    throw new IllegalArgumentException("change of size 0 is not allowed");
